95547b0f3494e68e95b411eb97146577b49be6d3
kush <kushaheer709@gmail.com>
2024-03-20 16:12:30 +0530

    Setup BootStrap in frontend

---
+++ Frontend/src/Components/RegisterComponent.jsx
@@ -0,0 +1,23 @@
+
+
+
+function RegisterComponent() {
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src=".assets\registerImage.svg" />
+                    </div>
+                    <div className="col-md-6 pt-5">
+                        <h1 className="fs-1">Register</h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default RegisterComponent
---
+++ Frontend/src/Pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import RegisterComponent from "../Components/RegisterComponent";
+
+function RegisterPage(){
+    return(
+        <div>
+            <RegisterComponent/>
+        </div>
+    )
+}
+
+export default RegisterPage;
