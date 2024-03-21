e7870613c380fb64e2cb70891f8a504e51d865d8
kush <kushaheer709@gmail.com>
2024-03-21 21:15:30 +0530

    Done Some Styling

--- Frontend/src/Components/RegisterComponent.jsx
+++ Frontend/src/Components/RegisterComponent.jsx
@@ -1,20 +1,61 @@
-
-
-
+import classes from './RegisterComp.module.css'
+import image from '../assets/Images/registerLogin.png'
 function RegisterComponent() {
+
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
+
+
+
     return (
         <div>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-6">
-                        <img src=".assets\registerImage.svg" />
-                    </div>
-                    <div className="col-md-6 pt-5">
-                        <h1 className="fs-1">Register</h1>
-                    </div>
-                </div>
-            </div>
+            <div className={classes.main}>
+                {/* <div className=''> */}
+                    <div className={classes.mainLeft}>
+                        {/* <div className='col-md-6'> */}
+                            <div className={classes.registerWrap}>
+                                <div className={classes.registerForm}>
+                                    <div className={"row"}>
+                                        <div className='col-md-12'>
+                                            <h1>Register</h1>
+                                        </div>
+                                        <form onSubmit={submitHandler}>
+                                            <div className='col-md-12'>
+                                                <label htmlFor='name'>Full Name</label>
+                                                <input className={classes.input} type='text' id='name' name='name' placeholder='Full Name' />
+                                            </div>
+                                            <div className='col-md-12'>
 
+                                                <label htmlFor='userName'>User Name</label>
+                                                <input className={classes.input} type='text' id='userName' name='userName' placeholder='User Name' />
+                                            </div>
+                                            <div className='col-md-12'>
+
+                                                <label htmlFor='email'>Email</label>
+                                                <input className={classes.input} type='email' id='email' name='email' placeholder='Email' />
+                                            </div>
+                                            <div className='col-md-12'>
+                                                <label htmlFor='password'>Password</label>
+                                                <input className={classes.input} type='password' id='password' name='password' placeholder='Password' />
+                                            </div>
+                                            <button className={classes.registerButton} type='submit'>Register</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        {/* </div> */}
+                    </div>
+                    <div className={classes.mainRight}>
+                        <div className={`col-md-6 ${classes.imageBackground}`}>
+                            <img src={image} />
+                        </div>
+                    </div>
+
+                    
+                {/* </div> */}
+            </div>
         </div>
     )
 
--- Frontend/src/Pages/RegisterPage.jsx
+++ Frontend/src/Pages/RegisterPage.jsx
@@ -3,9 +3,9 @@ import RegisterComponent from "../Components/RegisterComponent";
 function RegisterPage(){
     return(
         <div>
-            <RegisterComponent/>
+            <RegisterComponent />
         </div>
     )
 }
 
-export default RegisterPage;
+export default RegisterPage
