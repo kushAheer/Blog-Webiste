a3554c51a2a804dd85a2923004f75227be1a3634
Kush Aheer <kushaheer709@gmail.com>
2024-04-24 22:27:38 +0530

    Added Authentication

---
+++ Backend/MiddleWares/Auth.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _config;
+
+    public JwtMiddleware(RequestDelegate next, IConfiguration config)
+    {
+        _next = next;
+        _config = config;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+        if (token != null)
+            AttachUserToContext(context, token);
+
+        await _next(context);
+    }
+
+    private void AttachUserToContext(HttpContext context, string token)
+    {
+        try
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+            // attach user to context on successful jwt validation
+            context.Items["User"] = userId;
+        }
+        catch
+        {
+            // do nothing if jwt validation fails
+            // user is not attached to context so request won't have access to secure routes
+        }
+    }
+}
